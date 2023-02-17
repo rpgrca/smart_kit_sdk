dde756a75f1ccd672be210016a7508ca10c52784
Roberto <rpgrca@gmail.com>
2023-02-17 19:30:55 -0300

    add second ping

--- sk.py
+++ sk.py
@@ -1,9 +1,10 @@
 import requests
 
-API_TOKEN = "3939E0F2-08E5-4BB1-B3A6-DDB1183666D3"
-API_URL_BASE = "https://bgh-services.solidmation.com/1.0/"
-
 class SmartKitConnection:
+    API_TOKEN = "3939E0F2-08E5-4BB1-B3A6-DDB1183666D3"
+    API_URL_BASE = "https://bgh-services.solidmation.com/1.0/"
+    API_URL = API_URL_BASE + "HomeCloudServiceAdmin.svc/"
+
     def __init__(self):
         self.__session = requests.Session()
 
@@ -15,18 +16,26 @@ class SmartKitConnection:
         return headers
 
     def _post(self, command, **kwargs):
-        return self.__session.post(API_URL_BASE + command,
+        return self.__session.post(self.API_URL + command,
                                    headers=self._get_http_headers(), **kwargs)
 
     def _get(self, command, **kwargs):
-        return self.__session.get(API_URL_BASE + command,
+        return self.__session.get(self.API_URL_BASE + command,
                                   headers=self._get_http_headers(), **kwargs)
 
     def ping(self):
         result = self._get("HomeCloudCommandService.svc")
         return result.status_code == 200
 
+    def ping2(self):
+        result = self._post("Ping", data=f"token={self.API_TOKEN}")
+        return result
+
+    def login(self, email, password):
+        result = self._post("Login", data=f"token={self.API_TOKEN}&eMail={email}&password={password}")
+        print(result.content)
+        return result
 
 sk = SmartKitConnection()
-print(sk.ping())
-
+result = sk.ping2()
+print(result.content)
