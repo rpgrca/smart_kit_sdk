1bfcfcad955832e3abc5931d2585171f859c3136
Roberto <rpgrca@gmail.com>
2023-02-22 11:03:54 -0300

    simplify code

--- sk.py
+++ sk.py
@@ -21,27 +21,25 @@ class SmartKitConnection:
 
         return result.get("PingResult")
 
-    def _post_at_HomeCloudService(self, service: str, token: str, root: str, params: dict[str, str] = {}):
+    def _post_at_HomeCloudService(self, service: str, token: str, params: dict[str, str] = {}, root: str = None):
         data = json.dumps({ "token": { "Token": token }} | params)
         result = self.__connector.post_at_HomeCloudService(service, data=data)
         if result is None:
             return None
 
-        return result.get(root)
+        if root is None:
+            return result.get(f"{service}Result")
+        else:
+            return result.get(root)
 
     def ping_with_token(self):
-        return self._post_at_HomeCloudService("PingWithToken", self.__token, "PingWithTokenResult")
+        return self._post_at_HomeCloudService("PingWithToken", self.__token)
 
     def get_account(self):
-        return self._post_at_HomeCloudService("GetAccount", self.__token, "GetAccountResult")
+        return self._post_at_HomeCloudService("GetAccount", self.__token)
     
     def check_for_new_firmware_available(self, home_id):
-        data = json.dumps({ "token": { "Token": self.__token }, "homeID": home_id })
-        result = self.__connector.post_at_HomeCloudService("CheckForNewFirmwareAvailable", data=data)
-        if result is None:
-            return None
-
-        return result.get("CheckForNewFirmwareAvailableResult")
+        return self._post_at_HomeCloudService("CheckForNewFirmwareAvailable", self.__token, { "homeID": home_id })
 
     def login(self, email, password):
         data = json.dumps({ "token": { "Token": self.APPLICATION_TOKEN }, "eMail": email, "password": password })
@@ -59,29 +57,19 @@ class SmartKitConnection:
         return result.json().get("SendForgottenPasswordEmailResult")
 
     def get_login_details(self):
-        data = json.dumps({ "token": { "Token": self.__token } })
-        result = self.__connector.post_at_HomeCloudService("GetLoginDetails", data=data)
-        return result.json().get("GetLoginDetailsResult")
+        return self._post_at_HomeCloudService("GetLoginDetails", self.__token, "GetLoginDetailsResult")
 
     def get_firmware_information(self, device):
-        data = json.dumps({ "token": { "Token": self.__token }, "deviceModelDescription": device })
-        result = self.__connector.post_at_HomeCloudService("GetFirmwareInformation", data=data)
-        return result.json().get("GetFirmwareInformationResult")
+        return self._post_at_HomeCloudService("GetFirmwareInformation", self.__token, { "deviceModelDescription": device })
 
     def get_firmware_information_for_all_models(self):
-        data = json.dumps({ "token": { "Token": self.__token } })
-        result = self.__connector.post_at_HomeCloudService("GetFirmwareInformationForAllModels", data=data)
-        return result.json().get("GetFirmwareInformationForAllModelsResult")
+        return self._post_at_HomeCloudService("GetFirmwareInformationForAllModels", self.__token)
 
     def get_group(self, group_id):
-        data = json.dumps({ "token": { "Token": self.__token }, "groupID": group_id })
-        result = self.__connector.post_at_HomeCloudService("GetGroup", data=data)
-        return result.json().get("GetGroupResult")
+        return self._post_at_HomeCloudService("GetGroup", self.__token, { "groupID": group_id })
 
     def get_home(self, home_id):
-        data = json.dumps({ "token": { "Token": self.__token }, "homeID": home_id })
-        result = self.__connector.post_at_HomeCloudService("GetHome", data=data)
-        return result.json().get("GetHomeResult")
+        return self._post_at_HomeCloudService("GetHome", self.__token, { "homeID": home_id })
 
     def get_endpoint_usage_records(self, home_id, endpoint_id, date_from, date_to, time_to, language = "ES", width = 640, height = 480, color = 0xff0000, query_type = 0, email = None):
         usage = {
@@ -98,14 +86,11 @@ class SmartKitConnection:
             "CustomerColorAsHex": color,
             "EmailAddressForDownload": email
         }
-        data = json.dumps({ "token": { "Token": self.__token }, "parameters": usage })
-        result = self.__connector.post_at_HomeCloudService("GetEndpointUsageRecords", data=data)
-        return result.json().get("GetEndpointUsageRecordsResult")
+        
+        return self._post_at_HomeCloudService("GetEndpointUsageRecords", self.__token, { "parameters": usage })
 
     def get_scene(self, scene_id):
-        data = json.dumps({ "token": { "Token": self.__token }, "sceneID": scene_id })
-        result = self.__connector.post_at_HomeCloudService("GetScene", data=data)
-        return result.json().get("GetSceneResult")
+        return self._post_at_HomeCloudService("GetScene", self.__token, { "sceneID": scene_id })
 
 
 if __name__ == "__main__":
@@ -113,7 +98,7 @@ if __name__ == "__main__":
     sk = SmartKitConnection(ACCESS_KEY, connector)
 #    result = sk.get_endpoint_usage_records(HOME_ID, ENDPOINT_ID, { "Day": 17, "Month": 2, "Year": 2023 }, { "Day": 18, "Month": 2, "Year": 2023 }, { "Hour": 23, "Minute": 59, "Second": 59, "Millisecond": 0 }, query_type = 1)
 
-    result = sk.ping_with_token()
+    result = sk.check_for_new_firmware_available(HOME_ID)
     print(result)
 
 # vim:ts=4:nowrap
